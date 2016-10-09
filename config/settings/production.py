6281a59d613f024094d74e843306a52bdf1fc50c
Sam Collins <theatrepro11@gmail.com>
2016-10-09 13:51:07 +0100

    Made Media URL use ENV for CDN

--- config/settings/production.py
+++ config/settings/production.py
@@ -113,8 +113,10 @@ AWS_HEADERS = {
 
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
-MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
-
+MEDIA_URL = 'https://{}/{}/'.format(
+    env('AWS_MEDIA_CDN', 's3.amazonaws.com'),
+    AWS_STORAGE_BUCKET_NAME
+)
 
 # Static Assets
 # ------------------------
