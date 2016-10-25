a4a12ab0cb5f789368c71c56966dbc5fc61585e1
Sam Collins <theatrepro11@gmail.com>
2016-10-25 23:43:51 +0100

    Added reading handling and number model

--- sensor_portal/nexmo/views.py
+++ sensor_portal/nexmo/views.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from sensor_portal.sensors.models import Metric, Reading
+
 from .nexmo import verify_webhook
 from .models import Message
 
@@ -26,6 +28,28 @@ def webhook(request):
         return HttpResponse('Invalid Request')
 
     message.save()
+
+    metrics = Metric.objects.all()
+    values = message.text.split(',')
+
+    if len(values) != len(metrics) + 1:
+        return HttpResponse('There are not enough values in that text...')
+
+    index = 0
+    readings = []
+    sensor = Sensor.object.get(pk=values[index])
+
+    for metric in metrics:
+        readings.append(Reading(
+            sensor = sensor.id,
+            message = message.id,
+            metric = metric.id,
+            value = values[index],
+        ))
+        index += 1
+
+    print(readings)
+    Reading.objects.bulk_create(readings)
 
     return HttpResponse('Thanks Nexmo :)')
 
