09bfb1c895db3cc62e07b08a60102362a558f4cc
Sam Collins <sam@scollins.co>
2016-11-28 23:20:57 +0000

    Started Hidden Readings and PyCharm inspired code cleanups

--- config/urls.py
+++ config/urls.py
@@ -9,10 +9,12 @@ from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from django.template import Context, loader
-from django.views.defaults import page_not_found, server_error
 from django.http import HttpResponseServerError
 
-from sensor_portal.sensors.views import SensorViewSet, MetricViewSet, ReadingViewSet, sensor_map, sensor_list, sensor_metrics
+from sensor_portal.sensors.views import (
+    SensorViewSet, MetricViewSet, ReadingViewSet, sensor_map, sensor_list,
+    sensor_metrics
+)
 from rest_framework import routers
 
 admin.site.site_header = 'Sensor Panel Admin'
@@ -23,36 +25,36 @@ router.register(r'metrics', MetricViewSet)
 router.register(r'readings', ReadingViewSet)
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
-    url(r'^pages/', include('django.contrib.flatpages.urls')),
+                  url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+                  url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+                  url(r'^pages/', include('django.contrib.flatpages.urls')),
 
-    url(r'^map/$', sensor_map, name='sensor-map'),
-    url(r'^sensors/$', sensor_list, name='sensor-list'),
-    url(r'^metrics/(?P<id>[0-9]+)/$', sensor_metrics, name='sensor-metrics'),
+                  url(r'^map/$', sensor_map, name='sensor-map'),
+                  url(r'^sensors/$', sensor_list, name='sensor-list'),
+                  url(r'^metrics/(?P<id>[0-9]+)/$', sensor_metrics, name='sensor-metrics'),
 
-#    url(r'{}/doc/'.format(settings.ADMIN_URL), include('django.contrib.admindocs.urls')).
+                  url(r'{}'.format(settings.ADMIN_URL), admin.site.urls),
+                  url(r'{}django-ses/'.format(settings.ADMIN_URL), include('django_ses.urls')),
+                  url(r'{}docs/'.format(settings.ADMIN_URL), include('django.contrib.admindocs.urls')),
 
-    # Django Admin, use {% url 'admin:index' %}
-    url(r'{}'.format(settings.ADMIN_URL), admin.site.urls),
-    url(r'{}django-ses/'.format(settings.ADMIN_URL), include('django_ses.urls')),
+                  url(r'{}password_reset/$'.format(settings.ADMIN_URL), auth_views.password_reset,
+                      name='admin_password_reset'),
+                  url(r'{}password_reset/done/$'.format(settings.ADMIN_URL), auth_views.password_reset_done,
+                      name='password_reset_done'),
+                  url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm,
+                      name='password_reset_confirm'),
+                  url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
-    url(r'{}password_reset/$'.format(settings.ADMIN_URL), auth_views.password_reset, name='admin_password_reset'),
-    url(r'{}password_reset/done/$'.format(settings.ADMIN_URL), auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+                  # User management
+                  url(r'^users/', include('sensor_portal.users.urls', namespace='users')),
+                  url(r'^accounts/', include('allauth.urls')),
+                  url(r'^', include('allauth_2fa.urls')),
 
-    # User management
-    url(r'^users/', include('sensor_portal.users.urls', namespace='users')),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^', include('allauth_2fa.urls')),
+                  url(r'^api/v1/', include(router.urls, namespace='v1')),
+                  url(r'^invitations/', include('invitations.urls', namespace='invitations')),
+                  url(r'^nexmo/', include('sensor_portal.nexmo.urls', namespace='nexmo')),
 
-    url(r'^api/v1/', include(router.urls, namespace='v1')),
-    url(r'^invitations/', include('invitations.urls', namespace='invitations')),
-    url(r'^nexmo/', include('sensor_portal.nexmo.urls', namespace='nexmo')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
@@ -69,11 +71,8 @@ if settings.DEBUG:
             url(r'^__debug__/', include(debug_toolbar.urls)),
         ]
 
-def handler500(request):
-    """500 error handler which includes ``request`` in the context.
-    :template:`500.html`
-    """
 
+def handler500(request):
     t = loader.get_template('500.html')
     return HttpResponseServerError(t.render(Context({
         'request': request,
--- sensor_portal/sensors/serializers.py
+++ sensor_portal/sensors/serializers.py
@@ -4,21 +4,18 @@ from sensor_portal.sensors.models import Sensor, Metric, Reading
 
 
 class SensorSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Sensor
-        fields = ('name', 'position', 'active','description')
+        fields = ('name', 'position', 'active', 'description')
 
 
 class MetricSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Metric
         fields = ('name', 'unit', 'eu_limit', 'description')
 
 
 class ReadingSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Reading
-        fields = ('sensor', 'message', 'metric', 'value', 'recorded')
+        fields = ('sensor', 'metric', 'value', 'recorded')
