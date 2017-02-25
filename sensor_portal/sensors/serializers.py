2f1bbc600590a31114c09186cbd0bf6c18837321
Sam Collins <sam@scollins.co>
2017-02-25 00:47:43 +0000

    Switched to ModelSerializer from HyperlinkedModelSerializer

--- sensor_portal/sensors/serializers.py
+++ sensor_portal/sensors/serializers.py
@@ -3,19 +3,19 @@ from rest_framework import serializers
 from sensor_portal.sensors.models import Sensor, Metric, Reading
 
 
-class SensorSerializer(serializers.HyperlinkedModelSerializer):
+class SensorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ('name', 'position', 'active', 'description')
 
 
-class MetricSerializer(serializers.HyperlinkedModelSerializer):
+class MetricSerializer(serializers.ModelSerializer):
     class Meta:
         model = Metric
         fields = ('name', 'unit', 'eu_limit', 'description')
 
 
-class ReadingSerializer(serializers.HyperlinkedModelSerializer):
+class ReadingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reading
         fields = ('sensor', 'metric', 'value', 'recorded')
